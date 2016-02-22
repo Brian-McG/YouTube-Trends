c8c8197b96471e185d693cbca3f1a88b737978f3
Bryce Billing <bllbry001@myuct.ac.za>
2016-02-22 21:24:16 +0200

    Twit uses api 1.1

--- Twit.py
+++ Twit.py
@@ -4,6 +4,8 @@ __author__ = 'bryce'
 from TwitterSearch import *
 
 try:
+    config = {}
+    execfile("config.py", config)
     searchOrder = TwitterSearchOrder() # create a TwitterSearchOrder object
     searchOrder.set_keywords(['Happy'])
     searchOrder.set_count(10) # only return 10 pages
@@ -13,12 +15,8 @@ try:
     # Now let's create a Twitter Search API Object here
     # complete these by copying from your Twitter Application
     # from Twitter Developer Site
-    ts = TwitterSearch(
-                       consumer_key = 'UYfEFhbvL1KjRwbEJui7seAgt',
-                       consumer_secret = '9hjAXqlOxqftUbIZIcVtyuDSpdYAxq6K11x2yiWax2XtlpbFZA',
-                       access_token = '2517247723-O9nY8k2ExGNqSN2u1GIGy2pUR8SAVgNUc0iBcTg',
-                       access_token_secret = 'eBYu6Rof7UrkXCLK6uOlhULYSJLN0Wl6POUFFx1SnXX0E'
-                       )
+    ts = TwitterSearch(config["consumer_key"], config["consumer_secret"], config["access_key"], config["access_secret"])
+                       
 
     for happy_tweet in ts.search_tweets_iterable(searchOrder):
         print( '@%s tweeted: %s' % ( happy_tweet['user']['screen_name'],happy_tweet['text'] ) )
