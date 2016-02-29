2ef9c0a4f9f746531eaf1793897428d700f2f132
MCGBRI004-SeniorLab <mcgbri004@myuct.ac.za>
2016-02-29 18:34:20 +0200

    Add relative popularity

--- youtube_comment_parser.py
+++ youtube_comment_parser.py
@@ -9,6 +9,7 @@ REGION = "ZA"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 DATE_FORMAT = "%Y-%m-%d"
+START_DATE = datetime.date(2016, 02, 26)
 
 parser = argparse.ArgumentParser(description='Process collected youtube comments into a csv file')
 parser.add_argument('--video-id', '-vid', type=str, required=True, help='Video id to process')
@@ -22,8 +23,6 @@ with open('database_password.txt', 'r') as database_password_file:
 
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
-current_date = datetime.date(2016, 02, 26)
-
 videos_list_response = youtube.videos().list(
         id=args.video_id,
         regionCode=REGION,
@@ -36,14 +35,23 @@ publish_date = iso8601.parse_date(videos_list_response["items"][0]["snippet"]["p
 publish_date = datetime.date(publish_date.year, publish_date.month, publish_date.day)
 filename = "parsed_video_{0}_{1}.csv".format(args.video_id, time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()))
 sql_count_statement = """SELECT COUNT(id) from youtube_comments WHERE video_id = %s AND date_published BETWEEN %s AND %s"""
-# sql_count_statement = """SELECT * FROM youtube_comments where video_id = %s limit 10"""""
+max_count = -1;
+current_date = datetime.date(START_DATE)
+
+while current_date >= publish_date:
+    cursor.execute(sql_count_statement, (args.video_id, datetime.date.strftime(current_date, DATE_FORMAT+" 00:00:00"), datetime.date.strftime(current_date, DATE_FORMAT+" 23:59:59")))
+    count = cursor.fetchone()[0]
+    if count > max_count:
+        max_count = count
+
+current_date = datetime.date(START_DATE)
 with open(filename, 'w') as outfile:
-    outfile.write("date_published,comment_count\n")
+    outfile.write("date_published,comment_count,popularity_index\n")
 
     while current_date >= publish_date:
         cursor.execute(sql_count_statement, (args.video_id, datetime.date.strftime(current_date, DATE_FORMAT+" 00:00:00"), datetime.date.strftime(current_date, DATE_FORMAT+" 23:59:59")))
         count = cursor.fetchone()[0]
-        outfile.write("{0},{1}\n".format(datetime.date.strftime(current_date, DATE_FORMAT), count))
+        outfile.write("{0},{1},{2}\n".format(datetime.date.strftime(current_date, DATE_FORMAT), count, (count/max_count)*100) if max_count > 0 else 0)
         current_date = current_date - datetime.timedelta(days=1)
         print(current_date)
 
